255deadb55d5ba8bc2a12cbbd86de87dccee3a9d
cristian <calvarezmartinez.cf@iesesteveterrades.com>
2024-05-09 16:34:21 +0200

    daA

--- practica3/core/src/com/mygdx/game/GameScreen.java
+++ practica3/core/src/com/mygdx/game/GameScreen.java
@@ -1,123 +1,116 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Net;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.github.czyzby.websocket.WebSocketListener;
-import com.github.czyzby.websocket.WebSocket;
-import com.github.czyzby.websocket.WebSockets;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.Net.HttpRequest;
+import com.badlogic.gdx.Net.HttpResponse;
+import com.badlogic.gdx.Net.HttpResponseListener;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
 
 class GameScreen implements Screen {
-    private WebSocket socket;
-    private String address = "localhost";
-    private int port = 8888;
-    private float lastSendTime = 0f;
-    private static final float SEND_INTERVAL = 1.0f; // Send data every 1 second
     private SpriteBatch spriteBatch;
     private BitmapFont font;
-    private String message = "Enviando información";
+    private Stage stage;
+    private String message = "Presiona el botón";
 
     public GameScreen() {
-        if (Gdx.app.getType() == Application.ApplicationType.Android) {
-            address = "10.0.2.2";
-        }
-        socket = WebSockets.newSocket(WebSockets.toWebSocketUrl(address, port));
-        socket.setSendGracefully(false);
-        socket.addListener(new MyWSListener());
-        socket.connect();
-        socket.send("Enviar dades");
-
-        // Create SpriteBatch and BitmapFont
         spriteBatch = new SpriteBatch();
         font = new BitmapFont();
-        font.getData().setScale(2); // Scale the font up for visibility
+        font.getData().setScale(3); // Increase the font scale for better visibility
+
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        TextButtonStyle textButtonStyle = new TextButtonStyle();
+        textButtonStyle.font = font;
+        textButtonStyle.fontColor = Color.WHITE;
+
+        TextButton button = new TextButton("Obtener IP", textButtonStyle);
+
+        // Calculate button position and size
+        float buttonWidth = 300;
+        float buttonHeight = 100;
+        float buttonX = (Gdx.graphics.getWidth() - buttonWidth) / 2;
+        float buttonY = (Gdx.graphics.getHeight() - buttonHeight) / 2;
+
+        button.setPosition(buttonX, buttonY);
+        button.setSize(buttonWidth, buttonHeight);
+
+        button.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                HttpRequest httpRequest = new HttpRequest(Net.HttpMethods.GET);
+                httpRequest.setUrl("https://api.ipify.org?format=json");
+
+                Gdx.net.sendHttpRequest(httpRequest, new HttpResponseListener() {
+                    @Override
+                    public void handleHttpResponse(HttpResponse httpResponse) {
+                        JsonReader jsonReader = new JsonReader();
+                        JsonValue json = jsonReader.parse(httpResponse.getResultAsString());
+                        message = "Tu IP es: " + json.getString("ip");
+                    }
+
+                    @Override
+                    public void failed(Throwable t) {
+                        message = "Error: " + t.getMessage();
+                    }
+
+                    @Override
+                    public void cancelled() {
+                        message = "Llamada HTTP cancelada";
+                    }
+                });
+            }
+        });
+
+        stage.addActor(button);
     }
 
     @Override
     public void render(float delta) {
-        // Clear screen
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // Send data at regular intervals
-        lastSendTime += delta;
-        if (lastSendTime >= SEND_INTERVAL) {
-            lastSendTime -= SEND_INTERVAL;
-            socket.send("Enviar dades");
-        }
-
-        // Render text
         spriteBatch.begin();
-        font.draw(spriteBatch, message, 100, 150); // Change position as needed
-        font.getData().setScale(7); // Aumentar la escala a 3 para hacerlo más grande
-
+        font.draw(spriteBatch, message, 100, 150); // Adjust as needed to center or place the text
         spriteBatch.end();
+
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
     }
 
     @Override
-    public void show() {
-
-    }
+    public void show() {}
 
     @Override
     public void resize(int width, int height) {
-
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
-    public void pause() {
-
-    }
+    public void pause() {}
 
     @Override
-    public void resume() {
-
-    }
+    public void resume() {}
 
     @Override
-    public void hide() {
-
-    }
+    public void hide() {}
 
     @Override
     public void dispose() {
         spriteBatch.dispose();
         font.dispose();
-        socket.close(); // Don't forget to close the socket
-    }
-
-    class MyWSListener implements WebSocketListener {
-        @Override
-        public boolean onOpen(WebSocket webSocket) {
-            System.out.println("Opening...");
-            return false;
-        }
-
-        @Override
-        public boolean onClose(WebSocket webSocket, int closeCode, String reason) {
-            System.out.println("Closing...");
-            return false;
-        }
-
-        @Override
-        public boolean onMessage(WebSocket webSocket, String packet) {
-            System.out.println("Message: " + packet);
-            return false;
-        }
-
-        @Override
-        public boolean onMessage(WebSocket webSocket, byte[] packet) {
-            System.out.println("Message (byte[]): " + new String(packet));
-            return false;
-        }
-
-        @Override
-        public boolean onError(WebSocket webSocket, Throwable error) {
-            System.out.println("ERROR:" + error.toString());
-            return false;
-        }
+        stage.dispose();
     }
 }
